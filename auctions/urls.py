e88e4215bffebc3338d3c4bcaca92eb5b38bccce
rodvicj <rodvicj@gmail.com>
2023-08-30 18:43:29 +0800

    working on wishlist

--- auctions/urls.py
+++ auctions/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("create", views.new_list, name="new_list"),
     path("add_watchlist", views.add_watchlist, name="add_watchlist"),
     path("remove_watchlist", views.remove_watchlist, name="remove_watchlist"),
+    # TODO: change watchlist to wishlist
     path("watchlist", views.view_watchlist, name="view_watchlist"),
     path("category", views.view_category, name="view_category"),
     path("category/<str:category_name>", views.view_by_category_name, name="view_by_category_name"),
