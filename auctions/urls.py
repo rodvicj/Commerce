13390c26fe451c85e0c68442a0bfc4881254d926
rodvicj <rodvicj@gmail.com>
2023-09-27 12:32:43 +0800

    add todo for new branch

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = "auctions"
 urlpatterns = [
+    # TODO: change to rest api;
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
