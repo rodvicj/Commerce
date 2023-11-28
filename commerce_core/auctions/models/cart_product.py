4209ef5cdb6f2f5498edf1e45404c690a9816b24
rodvicj <rodvicj@gmail.com>
2023-11-28 13:03:58 +0800

    cart product

--- commerce_core/auctions/models/cart_product.py
+++ commerce_core/auctions/models/cart_product.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+from commerce_core.auctions.models import Product
 
-class CartProduct:
+
+class CartProduct():
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
-    buyer = models.ForeignKey("users.User", related_name="cart_products", on_delete=models.CASCADE)
-    product = models.ForeignKey("shop.Product", related_name="cart_products", on_delete=models.CASCADE)
+    buyer = models.ForeignKey(User, related_name="cart_products", on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name="cart_products", on_delete=models.CASCADE)
     quantity = models.PositiveBigIntegerField()
 
     class Meta:
--- commerce_core/auctions/models/product.py
+++ commerce_core/auctions/models/product.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -15,11 +16,7 @@ class Product(Item):  # type: ignore
     activation_status = models.CharField(
         choices=ActivationStatus.choices, max_length=6, default=ActivationStatus.DRAFT
     )
-    seller = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return (
-            f"Product ID: {self.pk} | "
-            f"Name: {self.name} | "
-            f"Price: {self.price_amount} {self.price_core.ticker}"
-        )
+        return (f"Product ID: {self.pk} | " f"Name: {self.name} | " f"Price: {self.price_amount}")
